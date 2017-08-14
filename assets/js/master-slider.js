207359cd49bdcdabec0e038aa600474b1f2173d1
mbabiarz <sspole@gmail.com>
2017-08-14 16:57:14 -0600

    reworking low hanging fruit and start slider

--- assets/js/master-slider.js
+++ assets/js/master-slider.js
@@ -19,4 +19,55 @@ var MasterSliderShowcase2 = function () {
 
     };
 
-}();        
+}();     
+
+var MasterSliderShowcase3 = function () {
+
+    return {
+
+        //Master Slider
+        initMasterSliderShowcase3: function () {
+		    var slider = new MasterSlider();
+		     
+		    slider.control('bullets', {autohide:false , overVideo:true, dir:'v', align:'left', space:10 , margin:00});
+		 
+		    slider.setup('homeslider' , {
+		        width           : 1366,
+                height          : 450,
+                minHeight       : 0,
+                space           : 0,
+                start           : 1,
+                grabCursor      : true,
+                swipe           : true,
+                mouse           : true,
+                keyboard        : true,
+                layout          : "fullwidth",
+                wheel           : true,
+                autoplay        : true,
+                instantStartLayers:true,
+                mobileBGVideo:false,
+                loop            : false,
+                shuffle         : false,
+                preload         : 0,
+                heightLimit     : true,
+                autoHeight      : false,
+                smoothHeight    : true,
+                endPause        : false,
+                overPause       : true,
+                fillMode        : "fill",
+                centerControls  : true,
+                startOnAppear   : false,
+                layersMode      : "center",
+                autofillTarget  : "",
+                hideLayers      : false,
+                fullscreenMargin: 0,
+                speed           : 20,
+                dir             : "v",
+                parallaxMode    : 'swipe',
+                view            : "fadeBasic"
+		    });
+        }
+
+    };
+
+}();
